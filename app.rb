5e4a79f5352956673805fe61f1e04ce7076f41e2
Kizito Njoku <nkizito57@gmail.com>
2020-01-11 22:51:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+    binding.pry
+  end 
+  
+  get '/newteam/:name' do
+    @has_field = params[:name]
+    @has_field
+    erb :newteam
+  end 
+  
+  post '/newteam' do 
+    erb :team
+  end 
 
 end
